be1e869db201850aaf1e152501ec3cdb4ffb9602
lsminter <lsminteris@gmail.com>
2017-10-08 14:28:22 -0700

    added code example files

---
+++ javascript-introducing-reduce-reducing-an-array-into-an-object/script.js
@@ -0,0 +1,28 @@
+//see CONSOLE!
+var votes = [
+  "angular",
+  "angular",
+  "react",
+  "react",
+  "react",
+  "angular",
+  "ember",
+  "react",
+  "vanilla"
+];
+
+var initialValue = {};
+
+var reducer = function(tally, vote) {
+  if (!tally[vote]) {
+    tally[vote] = 1;
+  } else {
+    tally[vote] = tally[vote] + 1;
+  }
+
+  return tally;
+};
+
+var result = votes.reduce(reducer, initialValue);
+
+console.log(result);
