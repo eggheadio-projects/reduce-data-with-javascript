7321902818cb3a4a2298ab4fe6c3003ad79fb834
Zac Jones <zacjones93@gmail.com>
2017-10-11 13:06:40 -0700

    updated console.log behavior

--- script.js
+++ script.js
@@ -57,18 +57,8 @@ var stars = input.reduce(function(acc, value) {
 
 var data = [1, 2, 3, 4, "5"];
 var sum = data.reduceRight(function(acc, value, index) {
-  console.log(index) || displayInPreview(index);
+  console.log(`Index: ${index}`)
   return acc + value;
 }, 0);
 
-console.log(sum) || displayInPreview(sum);
-
-
-
-// display in plunker preview
-function displayInPreview(string) {
-  var newDiv = document.createElement("div"); 
-  var newContent = document.createTextNode(string); 
-  newDiv.appendChild(newContent);
-  document.body.appendChild(newDiv)
-}
+console.log("Sum: ", sum)
